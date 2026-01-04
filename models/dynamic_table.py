812897ed0f29a63f0398a256d024f64cdc050665
Matu <agostonmate9@gmail.com>
2026-01-04 16:48:43 +0100

    missing ;

--- models/dynamic_table.py
+++ models/dynamic_table.py
@@ -13,6 +13,6 @@ class DynamicTable(DatabaseObject):
         self.target_lag = target_lag
 
     def create_ddl(self):
-            ddl = f"""CREATE OR REPLACE DYNAMIC TABLE {self.schema}.{self.name}\nTARGET_LAG = '{self.target_lag}'\nWAREHOUSE = {self.warehouse}\nAS\n\t{self.columns}\nFROM {self.sourceobject}
+            ddl = f"""CREATE OR REPLACE DYNAMIC TABLE {self.schema}.{self.name}\nTARGET_LAG = '{self.target_lag}'\nWAREHOUSE = {self.warehouse}\nAS\n\t{self.columns}\nFROM {self.sourceobject};
             """
             return ddl.strip() # strip() removes extra whitespace from the start/end
